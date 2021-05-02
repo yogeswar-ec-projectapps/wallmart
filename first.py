7fa6b8ece579bb7b232a2731fe245598a46eb92c
yogeswar007 <yogeswargajula007@gmail.com>
2021-05-02 18:38:47 +0530

    modified first.py

--- first.py
+++ first.py
@@ -1,1 +1,19 @@
-wewjbcckjgccbshvsdjvhgs
+hello welcome to DevOps
+
+echo "Please enter file name:
+
+read filename
+
+if [ -r $filename ]
+then
+echo "$filename have read permission"
+elif [ -w $filename ]
+then
+echo "$filename have write permissin"
+elif [ -e $filename ]
+then
+echo "$file have execution permission"
+else
+echo "please enter file present in shell directory"
+fi
+
